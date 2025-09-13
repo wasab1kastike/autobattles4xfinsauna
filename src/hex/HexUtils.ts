196565ceb23f6bf6a8831a67d0377911eb1a3736
wasab1kastike <arto.askala@gmail.com>
2025-09-13 18:37:43 +0300

    feat: expand hex grid utilities

--- src/hex/HexUtils.ts
+++ src/hex/HexUtils.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:330bcffb76d5da5c29c1ab96f8dd0addfa62c7706ac9d9cb56132f70fdf1393a
-size 1456
+oid sha256:4acbea64c2c1c5bad0777eedfa02061ae13a442b98303f9a4e046f762c5bf339
+size 1940
