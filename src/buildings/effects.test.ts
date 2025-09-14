18895d6768170f079b66f80eda257b308e785c7d
wasab1kastike <arto.askala@gmail.com>
2025-09-14 14:38:54 +0300

    feat: implement building effect listeners and tests

---
+++ src/buildings/effects.test.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:88d2231c40f7b1961f1d587a2c6d4a7950525b559c3978b25a6f9b642385a89f
+size 1482
--- src/core/GameState.ts
+++ src/core/GameState.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c63047cf273591a78d02021df420704e3449805ac8a616a40a1213bb01b8364
-size 6321
+oid sha256:c54539f51c1ae5ba5f6b5821bd0e5fc5401778da5aa745e6cddc902ffa4b6eba
+size 6392
