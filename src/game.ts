4fa8b95fc2bc8d73e8113254ca30fdaf7e74c5aa
wasab1kastike <arto.askala@gmail.com>
2025-09-14 10:57:54 +0300

    Test asset loading fallback

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:860d65775e604dd3de8f88ce8e5f868dc9ca33268601bac0f5c821518e296de0
-size 3488
+oid sha256:6cd0c1ae9b8fb9b68aa4fcf384de5a8fb2235eaf3b6d97e78c4cc16a3ac5bfca
+size 3617
---
+++ src/loader.test.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d381b41ca4a525fef45163832ba9dceedcf8dd35b6b9f0f52d9dd50503c4ba7
+size 2687
