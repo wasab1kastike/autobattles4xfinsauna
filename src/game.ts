a2157afe9c4b1c1f6a6564290701568f295fde27
wasab1kastike <arto.askala@gmail.com>
2025-09-14 20:15:14 +0300

    Add raider sprite and health-based tint

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b972ded82f049a4d19f60cf9e0ff42b07d200c468b15e5a3ef00dada88d76d15
-size 6776
+oid sha256:f2a616cfba89f8b9a9bd566989f40a0daa8c3f44866a6c3e719d878c3ee24a10
+size 7239
