c800a46761f95022166646550668b2569874bb66
wasab1kastike <arto.askala@gmail.com>
2025-09-14 14:12:43 +0300

    Persist building data

--- src/core/GameState.ts
+++ src/core/GameState.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:291d33a6f50022641cd48863189e8f90ce3d08e4da79e08fe03c1d0f75d3146a
-size 5119
+oid sha256:08b7397dd4d75fe32d7a12c1c4a96d6583d330c7a80954d7d0d122c72840a942
+size 6211
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15a662b2de5c833e0faab3577aafe451c09d7bfb23dca3b3b64744b80a97d78a
-size 5372
+oid sha256:0ba347063f81bed4ee2788f1619ec83ebecf719becdb0a43efb9b939b0543865
+size 5375
