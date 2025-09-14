9541ba97530088a332eef24e9fadcff68d4add66
wasab1kastike <arto.askala@gmail.com>
2025-09-14 19:53:25 +0300

    Derive build base path from package.json homepage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
+import pkg from './package.json' assert { type: 'json' };
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/autobattles4xfinsauna/' : '/',
+  base: command === 'build' && pkg.homepage
+    ? new URL(pkg.homepage).pathname
+    : '/',
   root: 'src',
   publicDir: '../public',
   build: {
