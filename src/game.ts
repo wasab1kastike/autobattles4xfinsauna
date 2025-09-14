b291760c78fc8108b86b37d8bf593902a66827bf
wasab1kastike <arto.askala@gmail.com>
2025-09-14 21:50:19 +0300

    refactor: remove initial spawn and manual movement

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ae9b2ceb2bdafbe842acc79c55f15f4529fabf8371b5313f26b811e43fc8f3e
-size 6450
+oid sha256:3516a373072e06becb3b9a5f15c2eb986b0149866e91b0bae01918630f8ab63d
+size 5989
