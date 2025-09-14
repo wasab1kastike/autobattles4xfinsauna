fc4bcb25e3d1e8e583e1fc8fed94b5de2af4ecce
wasab1kastike <arto.askala@gmail.com>
2025-09-14 20:03:22 +0300

    Add camera auto-framing and reveal integration

--- src/hexmap.ts
+++ src/hexmap.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1034d48f4aef9f0876c40ff2a723648f8ee7e19bb07577eed78e2ebb7ce7dca8
-size 4910
+oid sha256:5586b62dc3656a3408d24ee5000ef6dcf9ccbcef4cdc1e64540130d3b8286010
+size 5332
