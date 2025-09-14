f19ef0d88b85ecc73984ad3e34b4b72e540861b0
wasab1kastike <arto.askala@gmail.com>
2025-09-14 11:16:53 +0300

    Add targeting system and integrate with battle management

---
+++ src/ai/Targeting.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a2b5cf03a169103151386db92e3053ed0660665e90ed3ec7067878e5bbaed0c4
+size 1040
--- src/units/Unit.ts
+++ src/units/Unit.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:645f33499aaa12f5a834fbedf2b213cb2184eec53c22d33ab5d6056d5cec3812
-size 4406
+oid sha256:1cfe59243ac27e0888f140cb1165c2adcb86038df3876754f305a3c3dabacde0
+size 4459
