7b592f24f9a213a8d83fc09ad081260b5411e8f6
wasab1kastike <arto.askala@gmail.com>
2025-09-15 17:41:00 +0300

    feat: defer init until DOM ready

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b03ae250c01aeb5a4009e8ead0cb968c103c4abc6bb07c086da4d00f1ee498a3
-size 1851
+oid sha256:d597202e61e5e9aad8305c500f28d2753304d63e8f9c43dd621c508f1c41c58a
+size 1924
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fd6e60d5aa104c47f970b29bc9b0610ca5e19bd583f806e68a2022ff32424b0
-size 6346
+oid sha256:57c238ef1da830d94919def6c2c14182f4475440e9cd1b08ce20502c940e09c4
+size 6581
