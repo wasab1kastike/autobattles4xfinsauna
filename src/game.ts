cdb1fd512622cd07012234a30ad40f3e87c6e6ff
wasab1kastike <arto.askala@gmail.com>
2025-09-15 20:05:48 +0300

    Add polished tile and UI icons

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fbc7ad4da6eac0edb600e03177274e679b1e40d6fc5b4646a3705ae1b0b1525
-size 5273
+oid sha256:739340928ec6742e04cf8a14068400d02fb276272e559e439e36d84d50d00469
+size 7081
