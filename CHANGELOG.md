bd9a3e658bfb7443f6f5b1a5e65198f73f89cd5f
wasab1kastike <arto.askala@gmail.com>
2025-09-15 16:33:58 +0300

    feat: expose unit max health

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:756bb44209a2442a81169b759cb493da39b82582b0ebddfebaf9aa05f2d2b1b6
-size 1038
+oid sha256:c8a3093aad2927fd0b359125c8abffff6287bfd4f153ad2193e4f95882555b8c
+size 1105
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a09cd565db74835047512d27a6b389bfff08368b8a7345d8815bdf82d8f59a7
-size 6293
+oid sha256:1cc90449cbba4f6c27b98ef8aae883d40083ad088e5bc1671bf94ed86c070a8f
+size 6268
