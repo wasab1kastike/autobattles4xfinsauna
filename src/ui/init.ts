801a3c9cf43faa8c18a96fe8d48ec5caf5c0256c
wasab1kastike <arto.askala@gmail.com>
2025-09-15 20:47:02 +0300

    Stabilize touch gesture resets

--- src/ui/init.ts
+++ src/ui/init.ts
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3a4b0cb02addff602010647e6fad5c4aecc95253a587ddd36e307d7f9f43d3a9
-size 1128
+export { init, destroy, resizeCanvasToDisplaySize, screenToWorld, camera, MIN_ZOOM, MAX_ZOOM } from '../main.ts';
