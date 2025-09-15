ef96376f6a6d127dc895caaebb76be49b59d9a31
wasab1kastike <arto.askala@gmail.com>
2025-09-15 17:54:46 +0300

    refactor: import sprites directly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d597202e61e5e9aad8305c500f28d2753304d63e8f9c43dd621c508f1c41c58a
-size 1924
+oid sha256:c2c02a722ded3dd93e46f3be2b0afdd9e424c3d4481cd4333562e7eff7ed284f
+size 1980
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57c238ef1da830d94919def6c2c14182f4475440e9cd1b08ce20502c940e09c4
-size 6581
+oid sha256:cc8d38e89fab14114d1b5d823e6ad0cfd6f6d72af2568892448aeb9e575def7a
+size 6763
