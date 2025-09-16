8c725c59b203223e7bc042c4c550289c898802cd
wasab1kastike <arto.askala@gmail.com>
2025-09-16 14:32:30 +0300

    Refine HUD layout and sauna controls

--- src/ui/topbar.ts
+++ src/ui/topbar.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc9e7b755cf1ef95556b8bb62ff3f619818d431010611698f060e102326650ef
-size 5030
+oid sha256:42dd11c29438897851423c2def499fe74c0d0d4af4b6ac2e26fcddbee4509f71
+size 5122
