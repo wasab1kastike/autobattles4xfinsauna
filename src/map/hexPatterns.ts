977b49dddd71691621858246a86b244e6de23676
wasab1kastike <arto.askala@gmail.com>
2025-09-16 17:59:03 +0300

    Add terrain patterns and dim fogged tiles

---
+++ src/map/hexPatterns.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c0cd79896d078b8e6e556fc545a541133509d2b58de7d064f0772fd3397c765
+size 7557
--- src/render/HexMapRenderer.ts
+++ src/render/HexMapRenderer.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b82d02a510b198daf3da6f96854c8ed5b16af5699f8f681be5280d5dc3a0efc
-size 6512
+oid sha256:0c67e45c4496c85b493dc8f25ae7ce18d9e725b1f4c38fb98beb9ef930869182
+size 7188
