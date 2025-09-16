d08c68a2e1309c5d31d6e1bb45b7150390a2d33a
wasab1kastike <arto.askala@gmail.com>
2025-09-16 12:50:06 +0300

    Add resource loader UI flow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb85fdf3834cb56bcaee81461a86626aa7c569fe2655f93d8abe2b9813f47407
-size 5888
+oid sha256:651cf2229fad4c65c9b75aa52b71dcdcd9111fa7f79357d75283f530379d8f38
+size 6065
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:549baaf0868c275289b6f28ddc68039fbe9249ba1cc4c32747efbf9cd642c685
-size 13113
+oid sha256:5756d44994a290e0033cc88066e744b1c1e84224fadc7f0275bb5d399662196c
+size 22414
