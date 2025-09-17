a7aa78a61c4f317455218656f04de24fbbe1a9bb
wasab1kastike <arto.askala@gmail.com>
2025-09-17 13:59:33 +0300

    Track Saunoja traits, upkeep, and XP

--- src/units/saunoja.ts
+++ src/units/saunoja.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69909c1637511e52dcc680b14f5052ee035f930a8978009ee36f644b452bc18b
-size 2232
+oid sha256:156b73884eab60e7d892ec3219b7fa5e60a3b9eb646c5d742e0e4e891cea2d73
+size 3354
