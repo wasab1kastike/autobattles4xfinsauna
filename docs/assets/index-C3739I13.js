3d1dac975114e20f15fb43a1048bc77d0250340a
wasab1kastike <arto.askala@gmail.com>
2025-09-17 12:47:32 +0300

    chore: refresh sauna beer production assets

---
+++ docs/assets/index-C3739I13.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9479b5fb5a01a4fbb01d206ba7bb6cf3797200092c29ae902465cee891dab063
+size 85016
--- docs/assets/index-DuOOUrfm.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:14fee0378010f9ef503e3088ccedd823a7f598b6230eba66bc3f67bd711d69a5
-size 67505
