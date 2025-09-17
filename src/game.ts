86c880e7b7e941f989e3ddd67608e41c6d85ff83
wasab1kastike <arto.askala@gmail.com>
2025-09-17 16:00:56 +0300

    Add enemy marauder spawner and edge spawn helper

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f2fc2ebd21043f29c796a813aa5d9467316eed8199fa5fabbc41799be7c7a8f
-size 17663
+oid sha256:1d0eea9923f1eb73ba6bf511b0e48defb89eff1daba9bc4871708ef756de5989
+size 18766
---
+++ src/sim/EnemySpawner.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a03e2346c05210dda4a9623430d3228bc65ee4cf1223138d91734096e226093
+size 2725
