5ab8d4fb7cd674e7c9f2cfa2744de0e6b4e9a81d
wasab1kastike <arto.askala@gmail.com>
2025-09-17 19:37:14 +0300

    Spawn enemies along fog frontier

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e28c4a88cc3630417acdb74c3d8a06ed420be77f5ee3b65998f84e9de6d6de21
-size 24407
+oid sha256:b9e440e7f0b3d6be5121a23c167911cee5a733a3eb9dcae65764e416cb79e621
+size 23439
