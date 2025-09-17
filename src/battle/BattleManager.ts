c10ff5d5d90ffd145089824a904f203f231d5df1
wasab1kastike <arto.askala@gmail.com>
2025-09-17 18:10:32 +0300

    Optimize unit pathfinding with caching

--- src/battle/BattleManager.ts
+++ src/battle/BattleManager.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbd770fb573cdd23abf2e5d4986b595bac90e5f38ba5edeb67c9a0905d0170e5
-size 2007
+oid sha256:4825f184dc64cb7badf89d9870f8b95b26d72c7457f83e0981d0cfa237aad9ae
+size 2894
--- src/units/Unit.ts
+++ src/units/Unit.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa2719cbccc6145a48db882409d0214a33d29f9441c9cc840df8ffa00f6af5e9
-size 7225
+oid sha256:46ee77088698fc5b2cc9f3c13a22230b1bd6f808a881dfc367975b9f438e5855
+size 8967
