9c4cd785ef333e499f9ec5a9db4f7402539415a0
wasab1kastike <arto.askala@gmail.com>
2025-09-18 08:05:49 +0300

    Refine spawn log messaging

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:190ee3bfbb35918332262357f184e178fe5056b467bc5a9ac6855675334b91b4
-size 25773
+oid sha256:20df6b5137cde8123cd8c3e8b502551bf7ba8faf20f0b1884eb07d2d5cc804e7
+size 26161
