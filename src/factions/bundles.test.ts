c35c6999ecc06f22be1e16744750703cdfa742b1
wasab1kastike <arto.askala@gmail.com>
2025-09-18 13:32:53 +0300

    feat: load faction spawn bundles

---
+++ src/factions/bundles.test.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ef5af50c1bd829cb74fd078aadfb05dbd93bf7af43d3f267ed269e9172421a1
+size 1197
---
+++ src/world/spawn/enemy_spawns.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:794dcc956c66ba8aa2258c844377d35b848578975d5a51f1c6727e84924705da
+size 2338
