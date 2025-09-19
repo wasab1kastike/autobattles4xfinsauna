f19bc4937220f1411b1fe7eab735221a6fbedfa5
wasab1kastike <arto.askala@gmail.com>
2025-09-19 08:11:53 +0300

    Add mobile viewport hook and responsive HUD controls

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c973a98c4ad3a6b07888296ce802d735e864acb81340812aa2ccacbe162bf686
-size 24024
+oid sha256:7310f39d4adf6dce9301e2279eaf09fd249d2b7702d5c08e0ce3dd9db93d28be
+size 24391
--- src/ui/rightPanel.tsx
+++ src/ui/rightPanel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14e94027d79852c1aee609acf535d15e54d03c44424d6e18ac4dcfd5ae70401e
-size 14438
+oid sha256:6b0b4b5d68d443585dfb91eef4202020d80b0138b280b323f1a60db6bb27ad2f
+size 21645
