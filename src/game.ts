3144dd08d6d47a16ed17b4d081838f2eb63cb59a
wasab1kastike <arto.askala@gmail.com>
2025-09-19 14:35:02 +0300

    Stop recurring camera auto-framing

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:676f41e5cef0e958d3d6ba8d4d1356c7efc53d47c042b0f55800f8d4d8673aa6
-size 45902
+oid sha256:642dc7d3bd7bcc58c5b96e93bf7a6787bfadb0e6d92aaac4f89484b2467e495a
+size 45946
