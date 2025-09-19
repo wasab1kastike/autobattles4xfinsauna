4e90cdcc7864704ad0333d6ecb0768d979c985c2
wasab1kastike <arto.askala@gmail.com>
2025-09-19 11:40:00 +0300

    refactor: move unit barrel into directory index

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf7a5f6674a49beb8680f11f066caecbdbbf721011ca45829e01e1c66527542a
-size 17522
+oid sha256:371d600caf8f28ca562288532ea14e8ea50e11b085b3bfd1b808069d07a3c003
+size 17546
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0af8ce8d3a9fed23f4038cba90287aec87147cc9051d1afa5d1d9f2fc59ecb3a
-size 45888
+oid sha256:49e82daec861899ce56922d15bd874a4fddef22e1e07af89812f9e71ecf4e1e9
+size 45900
