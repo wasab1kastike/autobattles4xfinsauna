7a272d95b6f876ad17a2ebd44605033ab3223277
wasab1kastike <arto.askala@gmail.com>
2025-09-19 10:32:16 +0300

    Seed starter roster with free upkeep

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:633a3939578e55124b4cb61984bca24f92b431cfb5878eeec0817592c91836ce
-size 16286
+oid sha256:cf7a5f6674a49beb8680f11f066caecbdbbf721011ca45829e01e1c66527542a
+size 17522
