816c5f6f3b18021410af6b195418b4f3737dd6f0
wasab1kastike <arto.askala@gmail.com>
2025-09-19 10:20:34 +0300

    Allow sauna beer debt to surface in HUD

---
+++ src/ui/topbar.test.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:08201241244c9c265f07af5008720e18cd34bef90400a3702d1740b08a3c679b
+size 1653
--- src/ui/topbar.ts
+++ src/ui/topbar.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92483048e0910b72e6eea960a938d7f1f87b8c056a9c37077ce50e4a6448c740
-size 19632
+oid sha256:f796c16bf35d9a313fcab148202f0ecb57990e5cea03ae374778a447fd9b68a5
+size 20522
