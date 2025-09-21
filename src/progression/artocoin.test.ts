0c21ffca54eba4616aa955f1081d418967b3d5a8
wasab1kastike <arto.askala@gmail.com>
2025-09-21 17:43:20 +0300

    Remove legacy docs bundle hashes

--- src/progression/artocoin.test.ts
+++ src/progression/artocoin.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9355c0b032f8f9a129beb4913477ae2544ef812898ba56a84e4cf7a66fe97b0
-size 3896
+oid sha256:4d0c033354b3c281226a1c9c994e66841f9ac884d0994007071e8cba83b5059d
+size 3939
