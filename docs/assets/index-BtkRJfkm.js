601ae3e7d5eaa308f6fbb65d16b619b4a1c9b32d
wasab1kastike <arto.askala@gmail.com>
2025-09-22 07:52:35 +0300

    Add selection mini HUD overlay for focused units

--- docs/assets/index-BtkRJfkm.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:982e28ef2336b52a2c3b0a3aaff2722a6b0a76812e6b718ea357c837feccb115
-size 741076
--- src/render/unit_fx.ts
+++ src/render/unit_fx.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8294c4bb6fccdb5d983d23843d6efa429e70a9a56b2a20844b6246dc275ba3ef
-size 9102
+oid sha256:5c02a067a863c7b570d857d4bc06ee8520992a3732bd793eaf694c33940c0d87
+size 11431
