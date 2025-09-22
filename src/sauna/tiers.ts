1b1cd418663a8de8abdd99cc81c7ffd1eeeada13
wasab1kastike <arto.askala@gmail.com>
2025-09-22 16:45:26 +0300

    Lower aurora and mythic sauna artocoin costs

--- src/sauna/tiers.ts
+++ src/sauna/tiers.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e732dfdb85545a1978e9433cd50fcf6bc2635c96afb925109e33d823f3755a11
-size 6346
+oid sha256:b5f66aad8266f941c1e62056b6965a4d00c00728846d576ee9ad14d67a910ed6
+size 6344
