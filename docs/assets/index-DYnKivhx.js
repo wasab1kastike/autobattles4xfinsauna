8e3fd14ab0a3dd5eb34d371d663b899e908842fe
wasab1kastike <arto.askala@gmail.com>
2025-09-24 08:58:59 +0300

    Fix stash panel mount and refresh docs

--- docs/assets/index-DYnKivhx.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e46867b8ee6b31f9ab5d83e6c263f882995ed05ff1c5cdb91bef5b35c161889b
-size 785727
--- src/ui/inventoryHud.ts
+++ src/ui/inventoryHud.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52cdc09d2f7fd52543bf8ab09683cc9a1b7d436b2c317a2d784edf28b6ddbfa2
-size 23534
+oid sha256:a1a6dec2ff7fd80e09c10a2e9a1780b7f6639af883244c88c6ab0ca381fadbab
+size 23226
