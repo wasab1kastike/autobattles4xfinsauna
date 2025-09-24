3502e5b05a0e06357bac4df50631bc0da760c10d
wasab1kastike <arto.askala@gmail.com>
2025-09-24 19:06:19 +0300

    Batch unit sprite rendering through atlas

--- src/render/renderer.ts
+++ src/render/renderer.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59b90c303a818341211cdb38362cb71a8f10900f3cd9bbc62ce10597ccb02d7d
-size 20644
+oid sha256:d6d61fd83be1d2ebfff23cdfaac8351696adedba7108f1efc69714c92b62e1c5
+size 24305
