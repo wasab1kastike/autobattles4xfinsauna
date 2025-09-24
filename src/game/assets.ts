b27cad0feb87441c82004e0b8afdd19dfd43e1af
wasab1kastike <arto.askala@gmail.com>
2025-09-24 20:51:06 +0300

    Swap Saunoja icons to new PNG art

--- src/game/assets.ts
+++ src/game/assets.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44a4e12696b875c57060829da51c0cf53a69c1ad61d66fac4355a32ff1dc1ffd
-size 2449
+oid sha256:ad60fde18b4417cd67003724668d21342d3dfb649f5955b08071d78c787fab61
+size 2500
