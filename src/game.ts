60b45e78b19c06575827f9bd18d55843443b1e10
wasab1kastike <arto.askala@gmail.com>
2025-09-25 12:37:21 +0300

    Revert "feat: randomize saunoja and orc spawn visuals"

--- src/game.ts
+++ src/game.ts
@@ -2257,7 +2257,7 @@ export function draw(): void {
               ? attendant.appearanceId
               : null;
           },
-          fallbackSpriteId: 'saunoja-02'
+          fallbackSpriteId: 'saunoja-guardian'
         }
       : undefined;
 
