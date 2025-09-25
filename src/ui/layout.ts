95b171a24a74c0090ae2fb3441f82e1e41aadfcc
wasab1kastike <arto.askala@gmail.com>
2025-09-25 15:07:23 +0300

    Sync docs build hash

--- src/ui/layout.ts
+++ src/ui/layout.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72c58ae4f2e4fd1f9802c97031a62a78e9e5d5ba8c4916ad86f0e9bbd05c73d0
-size 18188
+oid sha256:06e51d49182c345e7d9e58e683c415988cd7f95766df5d352303a3c74e7c9fd5
+size 8355
