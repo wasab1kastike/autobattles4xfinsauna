fd426ac122d69525199d362af5cbd0cba53744e8
wasab1kastike <arto.askala@gmail.com>
2025-09-25 10:05:33 +0300

    Harden build freshness and bundle ui icons

--- src/game/assets.ts
+++ src/game/assets.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:535c067a6606b07054d1ca3a00176a7823c6577d556f081a2bd9be6b2c5e6ca4
-size 2379
+oid sha256:8aef88444c1c6dd9b443dcace91ce38840de179d82268d1803595acd7392e210
+size 2389
