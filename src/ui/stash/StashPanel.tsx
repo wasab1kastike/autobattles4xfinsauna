6a77cf9da835d69f929f4cc5d5d57ef3a16e5502
wasab1kastike <arto.askala@gmail.com>
2025-09-25 06:32:15 +0300

    feat(ui): showcase sauna bucket art in stash panel

--- src/ui/stash/StashPanel.tsx
+++ src/ui/stash/StashPanel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7ea476fce74dee05f6ef19516b7374d14941f9db361b0b42d6f9ef09f53dd4b
-size 12460
+oid sha256:98068ef2c00f045ad34fe281bebb73a97b66f5663e7271cfed434a4a595438ba
+size 12660
