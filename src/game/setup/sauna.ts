219324b601c5f6549b4385f7f18a42afb7af83cb
wasab1kastike <arto.askala@gmail.com>
2025-09-27 15:57:27 +0300

    Refactor sauna setup lifecycle

---
+++ src/game/setup/sauna.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0f3a2a1f5cfbdb06a3721df76c70031572cb8021a5f451545b2acc05d30ceb4
+size 8511
---
+++ tests/setup/sauna.test.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf79f35e13fbca16a5d0dd6d5338cc30a18fd01cbb304b1850fc7fea6c98acb4
+size 4207
