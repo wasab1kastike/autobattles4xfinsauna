943f2fed0ab5cef554d516befa53370eaf35ebb9
wasab1kastike <arto.askala@gmail.com>
2025-09-27 14:05:17 +0300

    Revert "Revert "Implement combat policy modifiers and sync tests""

--- src/combat/resolve.ts
+++ src/combat/resolve.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecb4fb956f02514ef870243fb98adb50efbd6e85f4daa44498d7e3d431e278b9
-size 9322
+oid sha256:9fbe592d2bc8a2090e619f621a5439753111b37816924d7c61c8994f91acf9c1
+size 11169
