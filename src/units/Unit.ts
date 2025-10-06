6b2d97cd6903ebdb5ef46f74e989d05326e415b8
wasab1kastike <arto.askala@gmail.com>
2025-10-06 17:55:18 +0300

    Normalize spawned unit health

--- src/units/Unit.ts
+++ src/units/Unit.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ac0f3bb3bdc6f88e21ee7799773c99f282b63974e78c841d8a906a15c924647
-size 16250
+oid sha256:752b04791f92ebbde5f6a7947b8e2ac6a3aede535c8a5bdc4558acca04fcb4e8
+size 16504
--- src/units/UnitFactory.test.ts
+++ src/units/UnitFactory.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59b1efc403dae6c46cfbbcb1f6dc3a76b522d14ef1ff10636bd15f2e0bc1b908
-size 4505
+oid sha256:0ab951b27f1a80a87ccc10c908769a146fcb2fb5d793141b5256cfb7ed825372
+size 5423
