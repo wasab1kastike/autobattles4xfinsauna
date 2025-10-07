b8454b29ee26e7ce0733a7c9b3df17dab04153b5
wasab1kastike <arto.askala@gmail.com>
2025-10-07 21:09:32 +0300

    Ensure HUD overlay retains grid classes

--- src/ui/layout.ts
+++ src/ui/layout.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72166d14802a5803a0627ad8a8f6c3e52bf12a2738031b1eb00d1e911aa28179
-size 17808
+oid sha256:125b4aa1ee4fdcba062a69e62b251dc64306c8ea45b82ab8aa300ca2e576a809
+size 18341
