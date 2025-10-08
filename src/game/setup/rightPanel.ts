a2184b99bcb473bc5a5e93f8707a4c8494c61f49
wasab1kastike <arto.askala@gmail.com>
2025-10-08 07:19:09 +0300

    Refine HUD navigation and right panel views

--- src/game/setup/rightPanel.ts
+++ src/game/setup/rightPanel.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de47278856554a66078192b63eb5558d3878481482efbaca80d87aec2483f7d0
-size 2880
+oid sha256:7e6a5ac1d238629f3a79e95b1f6bbe9f41ddafd822f2077ac5838eaadf94e4fb
+size 3446
---
+++ src/ui/hudNavigation.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1f69efde6f8dd4897205fab33845b5357b0baddfb8811c02f0bd7dccaab0d17
+size 3298
