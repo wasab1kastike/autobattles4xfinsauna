552c0b662fd23363b023742cb2caf363a5ef8940
wasab1kastike <arto.askala@gmail.com>
2025-10-09 20:49:31 +0300

    Handle NaN roster counts in objective tracker

--- tests/progression/objectiveTracker.test.ts
+++ tests/progression/objectiveTracker.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d89b3b5620f7b5ba551c270b8b2199e2e4120488d9dd27cd0625ea8da9f7bd64
-size 2314
+oid sha256:b5a6196104ef65c179ce9016fcc139482b8115ea8a0f952e1253900c62e95539
+size 3054
