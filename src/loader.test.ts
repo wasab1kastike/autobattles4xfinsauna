854369bd984f852fc62f043b8ea2aa0876ea2225
wasab1kastike <arto.askala@gmail.com>
2025-10-09 07:10:48 +0300

    Fix asset loader base paths

--- src/loader.test.ts
+++ src/loader.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:412539f12c1650924a7694459763c435a5b7232cc2fefde043ce6e477181f1a0
-size 4685
+oid sha256:bce6b1d01c29ef5cba24980f18489526be3a7165c0ec65aad09bc74504c13499
+size 5896
