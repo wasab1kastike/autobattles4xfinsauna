482889ade4bff67f8a1e0dc1d4d77eea94abb1b9
wasab1kastike <arto.askala@gmail.com>
2025-10-11 12:14:38 +0300

    Add Sauna Skin defensive policy

--- src/economy/tick.test.ts
+++ src/economy/tick.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:567367b62af20091da0b74960b5aa3a6183fd890feceea11e202c36793fc8c5a
-size 9353
+oid sha256:8a4f9d354575a65b54d10a44d69031985efc3faf092020d94b3ffa4bed8f7a92
+size 9945
