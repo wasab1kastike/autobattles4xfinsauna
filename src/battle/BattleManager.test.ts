a3421da0dcc62c8ba02f2222cb1f24a51bf53f41
wasab1kastike <arto.askala@gmail.com>
2025-10-11 17:38:04 +0300

    Fix explorer idle fallback near sauna

--- src/battle/BattleManager.test.ts
+++ src/battle/BattleManager.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb695966f8378285b61c3a4b0c20006dd64e35054ea6a87cf5cfca9aaefeab4d
-size 16862
+oid sha256:a04c549e8e0a60aa06ff3aa43756521b1d2265c005303399152a524b348e35a1
+size 17598
--- src/battle/BattleManager.ts
+++ src/battle/BattleManager.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba86aa2b4a44da5447a4f103ccfe2ce48cb416ddaa42dfc7790cc4158cd7fb7d
-size 13138
+oid sha256:7709cef21dbe80b81be5dce31b743fbcc195bb42b9875bfab904e9a99506b934
+size 13363
