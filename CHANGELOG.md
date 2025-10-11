bf0aec16648a015d4033e09a856ca07d86b00eb1
wasab1kastike <arto.askala@gmail.com>
2025-10-11 19:02:58 +0300

    Seed and persist enemy strongholds

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0baa8359e1df83004722ff2c32c983da996c855a9e625ca4b0c9df46e139de77
-size 63563
+oid sha256:9c3c8d8d1fa3e5e429f188de7b1f2f915c518c813e3924d486c75bea450a1f76
+size 63857
--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac07c25956ffca45e5e162ecf1db58402801ffb5b8c8f88b62cd7c9dd0f9c4f3
-size 67679
+oid sha256:55ab5f69bff082005eb634d97b2f4048817414b3cd59cfc461bbcf722014f34d
+size 67892
