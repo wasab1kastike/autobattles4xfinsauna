3ee1f7ecd40875c4614d4815f662ae05935cb7d8
wasab1kastike <arto.askala@gmail.com>
2025-10-11 23:03:49 +0300

    feat: add Saunojas' Rage combat doctrine

--- src/combat/resolve.test.ts
+++ src/combat/resolve.test.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7e53e1548dffd296b431cc7169c4eb0c75b8aee42692b1906610ccc257dd1bc
-size 13740
+oid sha256:e2117aae2cb7f81e2fac1f51f4800ecd5c42ba9a4a39cec90b99cf38c817b84f
+size 14913
