ef0261136a46f15ea018568ced93b808d22aea42
wasab1kastike <arto.askala@gmail.com>
2025-10-11 11:33:02 +0300

    Gate loot rolls behind rarity unlocks

--- src/loot/roll.ts
+++ src/loot/roll.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca6cf7285496dc2e1c00e9d819611549490215f6a50ffdd1881a57b32cadd487
-size 4009
+oid sha256:b0c361de39745cbbb986cf9a6d0c2aae3be6e421db4ca277a02c9db2121e7f57
+size 4236
--- src/progression/lootUpgrades.ts
+++ src/progression/lootUpgrades.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8edc83f7d313495033656a687a43c9ebfef8616af11c182b4922009a075bf199
-size 5884
+oid sha256:3f5bfbc7b73f76002520d980a8f5186963122791cb46ccc1d0d8d4768bb069a0
+size 9393
