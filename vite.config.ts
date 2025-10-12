e1ab61e7f03eae03acc1270505b202cca5938ec4
wasab1kastike <arto.askala@gmail.com>
2025-10-12 21:13:10 +0300

    Fix GitHub Pages build metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { getShortCommitHash } from './build-info';
+
+const commitHash = getShortCommitHash();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
   base: '/', // custom domain at root
   plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  define: {
+    __COMMIT__: JSON.stringify(commitHash),
+  },
 });
