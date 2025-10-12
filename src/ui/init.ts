e1ef73dbc6b3f90f91cba1d37135537e83429216
wasab1kastike <arto.askala@gmail.com>
2025-10-12 19:26:56 +0300

    fix: only binaries tracked by Git LFS; renormalize

--- src/ui/init.ts
+++ src/ui/init.ts
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c4b8cd077dfa76e5d8024097c3fe1c6d2eaafd73020e6f878126bd2d513d9673
-size 115
+export { init, destroy, resizeCanvasToDisplaySize, screenToWorld, camera, MIN_ZOOM, MAX_ZOOM } from '../main.ts';
