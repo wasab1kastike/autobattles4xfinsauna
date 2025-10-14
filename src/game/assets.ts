b4c6c4c056003c4fd65503a81e7edeb4f226e582
wasab1kastike <arto.askala@gmail.com>
2025-10-14 19:54:30 +0300

    Resolve sprite asset URLs through Vite pipeline

--- src/game/assets.ts
+++ src/game/assets.ts
@@ -2,6 +2,15 @@ import saunaBeerIcon from '../../assets/ui/sauna-beer.svg';
 import saunaRosterIcon from '../../assets/ui/saunoja-roster.svg';
 import resourceIcon from '../../assets/ui/resource.svg';
 import soundIcon from '../../assets/ui/sound.svg';
+import barracksSprite from '../../assets/sprites/barracks.svg?url';
+import citySprite from '../../assets/sprites/city.svg?url';
+import farmSprite from '../../assets/sprites/farm.svg?url';
+import mineSprite from '../../assets/sprites/mine.svg?url';
+import enemyOrc1Sprite from '../../assets/units/enemy-orc-1.png?url';
+import enemyOrc2Sprite from '../../assets/units/enemy-orc-2.png?url';
+import saunoja01Sprite from '../../assets/units/saunoja-01.png?url';
+import saunoja02Sprite from '../../assets/units/saunoja-02.png?url';
+import saunoja03Sprite from '../../assets/units/saunoja-03.png?url';
 import { ARTOCOIN_CREST_PNG_DATA_URL } from '../media/artocoinCrest.ts';
 import type { AssetPaths, LoadedAssets } from '../loader.ts';
 
@@ -18,22 +27,22 @@ export const assetPaths: AssetPaths = {
   images: {
     placeholder:
       'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=',
-    'building-farm': '/assets/sprites/farm.svg',
-    'building-barracks': '/assets/sprites/barracks.svg',
-    'building-city': '/assets/sprites/city.svg',
-    'building-mine': '/assets/sprites/mine.svg',
-    'unit-soldier': '/assets/units/saunoja-02.png',
-    'unit-archer': '/assets/units/saunoja-03.png',
-    'unit-avanto-marauder': '/assets/units/enemy-orc-1.png',
-    'unit-marauder': '/assets/units/enemy-orc-1.png',
-    'unit-raider': '/assets/units/enemy-orc-1.png',
-    'unit-raider-captain': '/assets/units/enemy-orc-2.png',
-    'unit-raider-shaman': '/assets/units/enemy-orc-2.png',
-    'unit-enemy-orc-1': '/assets/units/enemy-orc-1.png',
-    'unit-enemy-orc-2': '/assets/units/enemy-orc-2.png',
-    'unit-saunoja': '/assets/units/saunoja-01.png',
-    'unit-saunoja-guardian': '/assets/units/saunoja-02.png',
-    'unit-saunoja-seer': '/assets/units/saunoja-03.png',
+    'building-farm': farmSprite,
+    'building-barracks': barracksSprite,
+    'building-city': citySprite,
+    'building-mine': mineSprite,
+    'unit-soldier': saunoja02Sprite,
+    'unit-archer': saunoja03Sprite,
+    'unit-avanto-marauder': enemyOrc1Sprite,
+    'unit-marauder': enemyOrc1Sprite,
+    'unit-raider': enemyOrc1Sprite,
+    'unit-raider-captain': enemyOrc2Sprite,
+    'unit-raider-shaman': enemyOrc2Sprite,
+    'unit-enemy-orc-1': enemyOrc1Sprite,
+    'unit-enemy-orc-2': enemyOrc2Sprite,
+    'unit-saunoja': saunoja01Sprite,
+    'unit-saunoja-guardian': saunoja02Sprite,
+    'unit-saunoja-seer': saunoja03Sprite,
     'icon-sauna-beer': uiIcons.saunaBeer,
     'icon-saunoja-roster': uiIcons.saunojaRoster,
     'icon-resource': uiIcons.resource,
